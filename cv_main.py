91ae8159e34299f74c5837e194dbddaf8a35b338
cesardesouza <cesarsouza.2002@icloud.com>
2025-04-21 19:42:21 -0300

    feat: added simulated annealing, despite some adjustments are needed

--- cv_main.py
+++ cv_main.py
@@ -3,23 +3,62 @@ from src.annealing_model import AnnealingModel
 import numpy as np
 import statistics
 import random
+import math
 from itertools import chain
 
+SA_Max = 10 #iterações para equilibrio termico
+TF = 0.1 #temperatura minima final
+alpha = 0.95 #taxa de decaimento
+# dados = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51]
+file = "formulas/eil51-tsp.txt"
+
+class SA():
+    def __init__(self):
+        self.T = 1
+        self.iterT = 0
+        self.nxy = cv_carregar_instancia(file)
+        self.matriz = cv_fazer_matriz_n_n(self.nxy)
+        self.solucoes = []
+        self.solucoes = cv_gerar_nova_solucao(self.solucoes, self.matriz)
+        self.atual = cv_calcular_distancia_total_da_solucao(self.solucoes[-1], self.matriz)
+        self.melhorSolucaoObtidaAteEntao = self.atual 
+
+    def run(self):
+        while (self.T>TF):
+            while (self.iterT < SA_Max) :
+                print(f"atual: {self.atual}\titerT: {self.iterT}")
+                self.iterT += 1
+                self.solucoes = cv_gerar_nova_solucao(self.solucoes, self.matriz)
+                vizinho = cv_calcular_distancia_total_da_solucao(self.solucoes[-1], self.matriz)
+
+                delta = vizinho - self.atual
+                if(delta < 0):
+                    self.atual = vizinho
+                    if(vizinho<self.melhorSolucaoObtidaAteEntao): self.melhorSolucaoObtidaAteEntao = vizinho
+                else:
+                    x = random.random()
+                    if(x < math.exp( -delta / self.T)):
+                        self.atual = vizinho
+            self.T *= alpha     
+            self.iterT = 0
+        return self.melhorSolucaoObtidaAteEntao
+    
+
+# def cv_main():
+#     file = "formulas/eil51-tsp.txt"
+#     nxy = cv_carregar_instancia(file)
+#     matriz = cv_fazer_matriz_n_n(nxy)
+#     solucoes = []
+
+#     for i in range(len(nxy)):
+
+#         solucoes = cv_gerar_nova_solucao(solucoes, matriz)
+
+#         distancia_total_placeholder = cv_calcular_distancia_total_da_solucao(solucoes[-1], matriz)
+#         print(f"Distancia total da solucao: {distancia_total_placeholder}")
 
 
-
-def cv_main():
-    file = "formulas/eil51-tsp.txt"
-    nxy = cv_carregar_instancia(file)
-    matriz = cv_fazer_matriz_n_n(nxy)
-    solucoes = []
-    solucoes = cv_gerar_nova_solucao(solucoes, matriz)
-
-    distancia_total_placeholder = cv_calcular_distancia_total_da_solucao(solucoes[-1], matriz)
-    print(f"Distancia total da solucao: {distancia_total_placeholder}")
-
-
-    return
+#     return
 
 def cv_carregar_instancia(file):
     """Carrega uma lista de pontos N no plano em, X e Y."""
@@ -119,7 +158,8 @@ def pitagoras(x1, y1, x2, y2) -> float:
 #     plt.close()
 
 if __name__ == "__main__":
-    cv_main()
+    sa = SA()
+    print(sa.run())
     # files = ["eil51-tsp.txt", "kroA100-tsp.txt"]
     # SA_MAXES = [1, 5, 10]
     # all_stats = {}  # file -> lista de 3 listas (uma por SA_MAX)
@@ -186,3 +226,44 @@ if __name__ == "__main__":
     #         f.write("\n")
 
 
+
+# import math
+# import random
+
+# SA_Max = 2 #iterações para equilibrio termico
+# TF = 0.1 #temperatura minima final
+# alpha = 0.95 #taxa de decaimento
+# dados = [50, 30, 45, 10, 60, 8, 25, 5, 30, 12, 6, 40, 3, 20, 2, 3, 5, 3, 6, 1, 0, 4, 5, 3, 23, 4, 5] 
+# file = "3-SAT-instances.zip/uf20-01.cnf"
+
+# class SA():
+#     def __init__(self):
+#         self.T = 1
+#         self.iterT = 0
+#         self.atual = random.choice(dados)
+#         self.melhorSolucaoObtidaAteEntao = self.atual
+
+#     def gerar_vizinho(self):
+#         return random.choice(dados)    
+
+#     def run(self):
+#         while (self.T>TF):
+#             while (self.iterT < SA_Max) :
+#                 print(f"atual: {self.atual}\titerT: {self.iterT}")
+#                 self.iterT += 1
+#                 vizinho = self.gerar_vizinho()
+#                 delta = vizinho - self.atual
+#                 if(delta < 0):
+#                     self.atual = vizinho
+#                     if(vizinho<self.melhorSolucaoObtidaAteEntao): self.melhorSolucaoObtidaAteEntao = vizinho
+#                 else:
+#                     x = random.random()
+#                     if(x < math.exp( -delta / self.T)):
+#                         self.atual = vizinho
+#             self.T *= alpha     
+#             self.iterT = 0
+#         return self.melhorSolucaoObtidaAteEntao
+    
+# if __name__ == "__main__":
+#     sa = SA()
+#     print(sa.run())
